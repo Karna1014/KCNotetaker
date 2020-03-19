fc49139e557e2ca92632efe1db1aecd2eed2b95c
Java K <karna.cutolo@gmail.com>
2020-03-19 09:11:51 -0400

    heroku create and readme final server

--- README.md
+++ README.md
@@ -1,2 +1,48 @@
 # KCNotetaker
-A place to save thoughts
+A place to save thoughts and to-dos.
+
+ 
+ ## Description: 
+ An express backend application that dynamically generates a note list. Utilizing user input, notes will be saved until the user deletes.
+ 
+ ## Table of Contents: 
+  * Installation 
+  * Usage 
+  * License 
+  * Contributing 
+  * Questions 
+  * Images 
+   
+   #### Installation: 
+
+   npm install
+   
+   ### Usage: 
+
+   As a user, I am always looking for ways to keep track of a lot of information.  This project will provide a resource for readily creating a generating a note list for maintaining written documentation of important items for recall later and eventual deletion when no longer pertinent.
+   
+   #### License: 
+
+   ISC: [![License: ISC](https://img.shields.io/badge/License-ISC-blue.svg)](https://opensource.org/licenses/ISC)
+   
+   ### Contributions: Karna Cutolo
+
+   ### Deployed Link:
+
+      
+  
+   ### Questions: 
+   
+   #### If you have any questions about this repo, please reach out: 
+
+   **Email: karna.cutolo@gmail.com**
+     
+   #### Images: 
+
+   **Responsiveness View - I-Phone 6/7/8**
+
+   ![Main Page](./public/assets/images/Notetaker-Resp.png)
+
+   **Full-Screen View**
+
+   ![Main Page](./public/assets/images/Notetaker-Full.png)
--- server.js
+++ server.js
@@ -1,7 +1,7 @@
 const express = require("express");
 const fs = require("fs");
 const path = require("path");
-const PORT = process.env.PORT || 3000;
+const PORT = process.env.PORT || 7000;
 
 
 
@@ -9,56 +9,77 @@ const app = express();
 // Sets up the express app to handle data parsing
 app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
-app.use(express.static('public'));
 
-// path1 = path.basename("../public/index.html");
-// path2 = path.basename("../public/notes.html");
+app.use(express.static(path.join(__dirname, 'public')));
 
-
-
-app.get('/', function(req, res) {
-  res.sendFile('index.html', { root: __dirname });
+app.get('/', function (req, res) {
+  res.send('/public/index.html', { root: __dirname });
 });
 
-  app.get("/notes", (req, res) => {
-    res.sendFile(path.join(__dirname, "/notes.html"));
-    
-  });
+app.get("/notes", (req, res) => {
+  res.sendFile(path.join(__dirname, "/public/notes.html"));
+});
+//url read and returned in JSON formate
+app.get("/api/notes", (req, res) => {
+  fs.readFile(path.join(__dirname, "db/db.json"), (error, data) =>{
+    if (error) {
+      throw error;
+    } else {
+      res.send(JSON.parse(data));
+    }
+  })
+});
 
-  app.get("/api/notes", (req, res) => {
-    const note = req.params.note;
-    console.log(note);
+//initial route to home page
+app.get("*", (req, res) => {
+  res.sendFile(path.join(__dirname, "/public/index.html"));
+});
 
-  app.get("*", (req, res) => {
-    // var path = require('path');
-    // var filePath = "./public/index.html"
-    // var resolvedPath = path.resolve(filePath);
-    // console.log(resolvedPath);
-    // return res.sendFile(resolvedPath);
-    res.sendFile(path.join(__dirname, "/index.html"));
-  });
+//method to receive info
+app.post("/api/notes", (req, res) => {
+  var notes = JSON.parse(fs.readFileSync(path.join(__dirname, "db/db.json"), (error, data) =>{
+    if (error) {
+      throw error;
+    }
+  }))
+//assign id to each item
+  var requestBody = req.body;
+  requestBody["id"] = (notes.length + 1).toString();
 
-    let found;
+  notes.push(requestBody);
 
-    characters.forEach(char => {
-      if(character === char.routeName) {
-        found = char;
-      }
-    });
-  
-    res.json(found || { success: false });
-  });
+  //new file created with id
+  fs.writeFileSync(path.join(__dirname, "db/db.json"), JSON.stringify(notes, null, 2), (error, data) => {
+    if (error) {
+      throw error;
+    }
+  })
 
-  app.post("/api/notes", (req, res) => {
-    const newNote = req.body;
-  
-    newNote.routeName = req.body.name.split(" ").join("").toLowerCase();
-  
-    notes.push(newNote);
-  
-    res.db.json(newNote);
-  });
-  
-  app.listen(PORT, () => {
-    console.log(`Server is listening port: ${PORT}`);
-  });
+  res.send(notes);
+});
+
+app.delete("/api/notes/:id", (req, res) => {
+  var notes = JSON.parse(fs.readFileSync(path.join(__dirname, "db/db.json"), (error, data) =>{
+    if (error) {
+      throw error;
+    }
+  }))
+
+  for (var i = 0; i < notes.length; i++) {
+    if (notes[i].id == req.params.id) {
+      notes.splice(i, 1);
+    }
+  }
+
+  fs.writeFileSync(path.join(__dirname, "db/db.json"), JSON.stringify(notes, null, 2), (error, data) => {
+    if (error) {
+      throw error;
+    }
+  })
+
+  res.send(notes);
+});
+
+app.listen(PORT, () => {
+  console.log(`Server is listening port: ${PORT}`);
+});
